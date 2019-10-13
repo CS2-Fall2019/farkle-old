9e12d6a3011db1d6f694c972a47bdf1eed298b21
james@jamesschulze.com <james@jamesschulze.com>
2019-10-12 22:47:17 -0500

    0.0.1

---
+++ farkle/Gameboard.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Gameboard.cs" company="Ian Burroughs, Mike Boudreau, Brandon Biles & James A. Schulze">
+//     Copyright (c) Ian Burroughs, Mike Boudreau, Brandon Biles & James A. Schulze. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace farkle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class representing the Gameboard.
+    /// </summary>
+    class Gameboard
+    {
+        /// <summary>
+        /// Field for the scoreboard. This may not be used.
+        /// </summary>
+        private int scoreBoard; 
+
+        /// <summary>
+        /// Field for the round of the game.
+        /// </summary>
+        private int round;
+
+        /// <summary>
+        /// Gets or sets the scoreboard. This may be deleted.
+        /// </summary>
+        public int ScoreBoard
+        {
+            get { return this.scoreBoard; }
+            set { this.scoreBoard = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the round.
+        /// </summary>
+        public int Round 
+        {
+            get { return this.round; }
+            set { this.round = value; }
+        }
+    }
+
+
+
+}
---
+++ farkle/Player.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Player.cs" company="Ian Burroughs, Mike Boudreau, Brandon Biles & James A. Schulze">
+//     Copyright (c) Ian Burroughs, Mike Boudreau, Brandon Biles & James A. Schulze. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace farkle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Player
+    {
+        // Playes total score.
+        private int score;
+
+        // Players score for the current round
+        private int pendingScore;
+
+        /// <summary>
+        /// Gets or sets Score.
+        /// </summary>
+        public int Score
+        {
+            get { return this.score; }
+            set { this.score = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets Pending Score.
+        /// </summary>
+        public int PendingScore
+        {
+            get { return this.pendingScore; }
+            set { this.pendingScore = value; }
+        }
+    }
+}
